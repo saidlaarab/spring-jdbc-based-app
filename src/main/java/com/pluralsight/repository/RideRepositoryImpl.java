e8fe18e2fbb923dd796bafa15d8c13308e426ec1
Said Laarab <said.laarab@capgemini.com>
2023-03-19 22:48:52 +0000

    Inserting new row using SimpleJdbcInsert-based approach

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.pluralsight.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -26,8 +29,29 @@ public class RideRepositoryImpl implements RideRepository {
 
 	@Override
 	public Ride createNewRide(Ride newRide) {
-		jdbcTemplate.update("INSERT INTO ride(name, duration) VALUES(?,?)", 
-				newRide.getName(), newRide.getDuration());
+		/*jdbcTemplate.update("INSERT INTO ride(name, duration) VALUES(?,?)", 
+				newRide.getName(), newRide.getDuration());*/
+		
+		SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+		// Define the table name to insert into, and columns whose values will provided
+		insert.setTableName("ride");
+		
+		List<String> columns = new ArrayList<>();
+		columns.add("name");
+		columns.add("duration");
+		insert.setColumnNames(columns);
+
+		// Define the data of the new line to be inserted:
+		Map<String, Object> rideData = new HashMap<>();
+		rideData.put("name", newRide.getName());
+		rideData.put("duration", newRide.getDuration());
+		
+		// Define the column whose value will generated automatically:
+		insert.setGeneratedKeyName("id");
+		
+		Number key = insert.executeAndReturnKey(rideData);
+		
+		System.out.println("generated key for the inserted Ride : " + key);
 		
 		return null;
 	}
